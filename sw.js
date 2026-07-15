cff2dbeaecfceeef2441dcd7cc5e6def89511acc
qafrast-sys <qafrast@gmail.com>
2026-07-15 10:01:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,22 +11,3 @@ self.addEventListener("fetch",event=>{
  }
  event.respondWith(caches.match(req).then(cached=>cached||fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));return res})));
 });
-const CACHE='qimam-portal-v2';
-const ASSETS=['/','/index.html','/manifest.webmanifest','/icon.svg'];
-
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
-});
-
-self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
-});
-
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin) return;
-  if(event.request.mode==='navigate'){
-    event.respondWith(fetch(event.request).catch(()=>caches.match('/index.html')));
-    return;
-  }
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
-});
